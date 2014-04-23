6b6335eea1f187ee338ce911a8a42c1844e54e47
joaoschaab <joao.schaab@gmail.com>
2014-04-23 15:37:25 -0300

    random position

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ main_feito.cpp Player.cpp PVector.cpp -o opengl1 -lGL -lstdc++ -lc -lm -lglut -lGLU
+g++ main.cpp Player.cpp PVector.cpp -o opengl1 -lGL -lstdc++ -lc -lm -lglut -lGLU
